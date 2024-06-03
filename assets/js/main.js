459e34348cb79da0e15ef721cf7bde76aeb9c570
Alex PC <alexyeh0911@gmail.com>
2024-06-03 08:14:12 +0800

    updated projects and link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Photon by HTML5 UP
+	Fractal by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -11,12 +11,12 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -26,7 +26,25 @@
 			}, 100);
 		});
 
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+		}
+
 	// Scrolly.
-		$('.scrolly').scrolly();
+		$('.scrolly')
+			.scrolly({
+				speed: 1500
+			});
 
 })(jQuery);
